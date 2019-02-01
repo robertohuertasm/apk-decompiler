76d9e44157cdcddc1acbf1f9dfd74c8bd0a46d5c
Roberto Huertas <roberto.huertas@outlook.com>
2019-02-01 12:09:16 +0100

    version adjustment

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::path::PathBuf;
 
 fn main() {
     let matches = App::new("apk decompiler")
-        .version("0.1.0")
+        .version("0.2.0")
         .about("A super simple utility to decompile your APKs.")
         .author("Roberto Huertas <roberto.huertas@outlook.com>")
         .arg(
